f677d18e04480440f91cabb0e8446474f8b7107e
winj04 <winj04@vse.cz>
2025-01-04 00:10:42 +0100

    vypisovani cviku na obrazovce

--- js/app.js
+++ js/app.js
@@ -1,4 +1,5 @@
-let counter = 1;
+let setCounter = 1;
+let excerciseCounter = 1;
 
 document.getElementById('save-excercise').addEventListener('click', function (){
   alert('nemas zadnou serii');
@@ -10,7 +11,7 @@ document.getElementById('add-set').addEventListener('click', function () {
 
   const outerGroup = document.createElement('div');
   outerGroup.classList.add('outer-div');
-  outerGroup.setAttribute('id', 'set' + counter);
+  outerGroup.setAttribute('id', 'set' + setCounter);
 
   const inputGroup = document.createElement('div');
   inputGroup.classList.add('input-group');
@@ -18,12 +19,12 @@ document.getElementById('add-set').addEventListener('click', function () {
   const repsInput = document.createElement('input');
   repsInput.setAttribute('type', 'number');
   repsInput.setAttribute('placeholder', 'Počet opakování');
-  repsInput.setAttribute('id', 'reps' + counter);
+  repsInput.setAttribute('id', 'reps' + setCounter);
 
   const weightInput = document.createElement('input');
   weightInput.setAttribute('type', 'number');
   weightInput.setAttribute('placeholder', 'Váha');
-  weightInput.setAttribute('id', 'weight' + counter);
+  weightInput.setAttribute('id', 'weight' + setCounter);
 
   const deleteButton = document.createElement('button');
   deleteButton.textContent = '-';
@@ -32,7 +33,7 @@ document.getElementById('add-set').addEventListener('click', function () {
 
   deleteButton.addEventListener('click', function () {
     outerGroup.remove();
-    counter--; // Snížení counteru, pokud dojde k odstranění setu
+    setCounter--; // Snížení counteru, pokud dojde k odstranění setu
   });
 
   inputGroup.appendChild(repsInput);
@@ -55,7 +56,7 @@ document.getElementById('add-set').addEventListener('click', function () {
       const sets = [];
 
       // Použití for smyčky k iteraci přes jednotlivé sety
-      for (let i = 1; i < counter; i++) {
+      for (let i = 1; i < setCounter; i++) {
         const reps = document.getElementById('reps' + i)?.value;
         const weight = document.getElementById('weight' + i)?.value;
 
@@ -84,14 +85,57 @@ document.getElementById('add-set').addEventListener('click', function () {
         return null;
       }
 
-
       console.log('Cvik:', exerciseName);
       console.log('Série:', sets);
+      saveExcercise(exerciseName, sets)
     });
 
     document.getElementById('workout-form').appendChild(addWorkout);
   }
 
-  counter++;
+  setCounter++;
 });
+
+
+
+function saveExcercise(excerciseName, sets)
+{
+  if (document.getElementById(excerciseName + excerciseCounter)) {
+    alert(excerciseName + ' si dnes uz cvicil!!!')
+    return null;
+  }
+  const workoutList = document.getElementById('workout-list')
+
+  document.createElement('section')
+
+
+  const title = document.createElement("div")
+  title.setAttribute('id', excerciseName + excerciseCounter)
+  title.classList.add('outer-div')
+
+  const deleteButton = document.createElement('button');
+  deleteButton.textContent = '-';
+  deleteButton.setAttribute('type', 'button');
+  deleteButton.classList.add('delete-button');
+  deleteButton.addEventListener("click", function (){
+    title.remove();
+  })
+
+  const excercise = document.createElement('h1')
+  excercise.textContent = excerciseName;
+  excercise.style.margin = '0 auto';
+
+  title.appendChild(excercise)
+  title.appendChild(deleteButton)
+
+  sets.forEach((set, index) => {
+    const setDetails = document.createElement('p');
+    setDetails.textContent = `Série ${index + 1}: Opakování - ${set.reps}, Váha - ${set.weight} kg`;
+    title.appendChild(setDetails);
+  });
+
+  workoutList.appendChild(title)
+}
+
+
 
