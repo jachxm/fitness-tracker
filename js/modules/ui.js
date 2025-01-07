2b43cb0372816b684599a381a81031458ec9babf
winj04 <winj04@vse.cz>
2025-01-07 21:39:57 +0100

    Nove files, nova tlacitka na prepinani mezi html soubory

--- js/modules/ui.js
+++ js/modules/ui.js
@@ -1,27 +1,60 @@
-export function getButton(buttonClass, id, text){
+//metoda pro vytvoření button
+export function getButton(buttonClass, id, text, title) {
   const button = document.createElement('button')
   button.setAttribute('type', 'button')
-  if (text){button.textContent = text}
-  if (buttonClass){button.classList.add(buttonClass)}
-  if (id){button.setAttribute('id', id)}
+  if (text) {
+    button.textContent = text
+  }
+  if (buttonClass) {
+    button.classList.add(buttonClass)
+  }
+  if (id) {
+    button.setAttribute('id', id)
+  }
+  if (title){
+    button.setAttribute('title', title)
+  }
   return button;
 }
 
-export function getInput(type, placeHolder, id, width){
+//metoda pro vytvoření input
+export function getInput(type, placeHolder, id, width, value) {
   const input = document.createElement('input')
-  if (type){input.setAttribute('type', type)}
-  if (placeHolder){input.setAttribute('placeholder', placeHolder)}
-  if (id){input.setAttribute('id', id)}
-  if (width){input.style.width = width}
+  if (type) {
+    input.setAttribute('type', type)
+  }
+  if (placeHolder) {
+    input.setAttribute('placeholder', placeHolder)
+  }
+  if (id) {
+    input.setAttribute('id', id)
+  }
+  if (width) {
+    input.style.width = width
+  }
+  if (value) {
+    input.value = value;
+  }
   return input
 }
 
-export function getDiv(divClass, id){
+//metoda pro vytvoření div
+export function getDiv(divClass, id) {
   const div = document.createElement('div')
-  if (divClass){div.classList.add(divClass)}
-  if (id){div.setAttribute('id', id)}
+  if (divClass) {
+    div.classList.add(divClass)
+  }
+  if (id) {
+    div.setAttribute('id', id)
+  }
   return div;
 }
 
-
+//metoda pro vytvoření paragragu p
+export function getP(id, text){
+  const p = document.createElement('p')
+  if (id){p.setAttribute('id', id)}
+  if (text){p.textContent = text}
+  return p;
+}
 
